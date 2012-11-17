eaed0232861bb631c9dad5a3c37fdc624701c4de
Tim Banks <thetimbanks@gmail.com>
2012-11-17 17:30:21 -0600

    Added a ping.js file that is used on external sites to log the tracking cookie

---
+++ js/ping.js
@@ -0,0 +1,6 @@
+var iframe = document.createElement("iframe");
+iframe.src = "http://shopstartup.local/ping/index.php";
+iframe.setAttribute("width", 0);
+iframe.setAttribute("height", 0);
+iframe.setAttribute("style", "display:none;");
+document.body.appendChild(iframe);
