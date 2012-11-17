3d7888ba8db8019d98448d1fa722eb536d54a2f2
Patrick Stoddart <prstoddart@gmail.com>
2012-11-17 14:48:42 -0600

    Added time variables in config

--- live/config.php
+++ live/config.php
@@ -11,5 +11,10 @@
 	// Set up database connection
 	$db['link'] = new mysqli($db['hostname'], $db['username'], $db['password'], $db['database']);
 	if(mysqli_connect_errno()){ die; }
+	
+	date_default_timezone_set("UTC");
+	$cfg['timestamp'] = mktime();
+	$cfg['sqltime'] = strftime("%Y-%m-%d %H:%M:%S",$cfg['timestamp']);
+
 
 ?>
