08e2a52490c4f2de95498028a311d6dc66a39a32
Patrick Stoddart <prstoddart@gmail.com>
2012-11-18 10:43:41 -0600

    data-dnt false Twitter

--- live/product/index.php
+++ live/product/index.php
@@ -103,7 +103,7 @@
                                     <a href="https://twitter.com/share" 
                                         class="twitter-share-button" 
                                         data-text="Check out <? echo urlencode($row1['productname']); ?> on Shop Startup!" 
-                                        data-via="shopstartup" data-dnt="true">Tweet</a>
+                                        data-via="shopstartup" data-dnt="false">Tweet</a>
                                         <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
                                     <br />
                                     <!-- Pin It Button -->
