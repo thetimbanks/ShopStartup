92fd3362842eb6e4c5548df71f0f0aa162774153
Patrick Stoddart <prstoddart@gmail.com>
2012-11-17 23:23:16 -0600

    Step1

--- live/businesses/index.php
+++ live/businesses/index.php
@@ -71,7 +71,7 @@
       <!-- Three columns of text below the carousel -->
       <div class="row">
         <div class="span4">
-          <img class="img-circle" src="http://placehold.it/140x140">
+          <img class="img-circle" src="<? echo $cfg['weburl']; ?>/img/steps/stepone.png">
           <h2>Heading</h2>
           <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
           <p><a class="btn" href="#">View details &raquo;</a></p>
