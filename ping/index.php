c97a4c2bd848d15a46b13639afc551b5407e3eb3
Patrick Stoddart <prstoddart@gmail.com>
2012-11-17 22:58:01 -0600

    Product

--- ping/index.php
+++ ping/index.php
@@ -8,9 +8,9 @@
 	$cookie = $_COOKIE["ShopStartup"];
 	
 	if(is_numeric($_GET['company'])){
-	// Add record to DB
-	$query1 = "INSERT INTO `purchases` SET `company` = '" .$_GET['company']. "', `session` = '" .$cookie. "', `datetime` = '" .$cfg['sqltime']. "'";
-	$db['link']->query($query1);
+		// Add record to DB
+		$query1 = "INSERT INTO `purchases` SET `product` = '" .$_GET['company']. "', `session` = '" .$cookie. "', `datetime` = '" .$cfg['sqltime']. "'";
+		$db['link']->query($query1);
 	}
 
 ?>
