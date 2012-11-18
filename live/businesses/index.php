f7524531e15bf46bf307aa2dabebf26192be793b
Patrick Stoddart <prstoddart@gmail.com>
2012-11-17 20:03:37 -0600

    Search update

--- live/businesses/index.php
+++ live/businesses/index.php
@@ -36,53 +36,7 @@
     ================================================== -->
     <!-- Wrap the .navbar in .container to center it on the page and provide easy way to target it with .navbar-wrapper. -->
     
-    <div class="centered">
-    	<img src="<?php echo $cfg['weburl']; ?>/img/logo.png" width="683" height="90" />
-    </div>
-    
-    <div class="container navbar-wrapper">
-      <div class="navbar navbar-inverse">
-        <div class="navbar-inner">
-          <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li><a href="<? echo $cfg['weburl']; ?>">Home</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Categories <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Food</a></li>
-                  <li><a href="#">Relationship</a></li>
-                  <li><a href="#">Entertainment</a></li>
-                  <li><a href="#">Beauty</a></li>
-                  <li><a href="#">Tech</a></li>
-                  <li><a href="#">Clothing</a></li>
-                 <!-- <li class="divider"></li>
-                  <li class="nav-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>-->
-                </ul>
-              </li>
-              <li class="active"><a href="<? echo $cfg['weburl']; ?>/businesses">For Startups</a></li>
-              <li><a href="#about">About Us</a></li>
-              <!-- Read about Bootstrap dropdowns at http://twitter.github.com/bootstrap/javascript.html#dropdowns -->
-            </ul>
-          </div><!--/.nav-collapse -->
-          <form action="" method="get">
-              <input type="text" class="search" value="Search"
-                    onfocus="if (this.value=='Search') {this.value = '';}" 
-                    onblur="if(this.value=='') {this.value = 'Search';}"
-                    maxlength="100" />
-          </form>
-        </div><!-- /.navbar-inner -->
-      </div><!-- /.navbar -->
-
-    </div><!-- /.container -->
+	<?php nav(); ?>
 
 
 
