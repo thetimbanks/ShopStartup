9dd5cee7189e41d08e5e93fb06ce4fa3f447bcfe
Patrick Stoddart <prstoddart@gmail.com>
2012-11-17 21:29:29 -0600

    Map

---
+++ live/map/index.php
@@ -0,0 +1,119 @@
+<?php require '../config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Shop Startup: Our Startups</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="<?php echo $cfg['weburl']; ?>/css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo $cfg['weburl']; ?>/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="<?php echo $cfg['weburl']; ?>/css/style.css" rel="stylesheet">
+    
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map_canvas { height: 100% }
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkJ9Y8dNxb9c5us-bMnG3jQ-sVuN2Fn5U&sensor=FALSE">
+    </script>
+    <script type="text/javascript">
+      function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+      }
+    </script>
+  </head>
+
+<body>
+
+
+
+    <!-- NAVBAR
+    ================================================== -->
+    <!-- Wrap the .navbar in .container to center it on the page and provide easy way to target it with .navbar-wrapper. -->
+    
+	<?php nav(); ?>
+
+    
+
+<div class="product-spacer"></div>
+    
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+    
+    	<div class="clearfix">
+
+            <?php
+                if(is_numeric($_GET['id'])){
+                    $query1 = "SELECT * FROM `products` WHERE `id` LIKE '" .addslashes($_GET['id']). "' LIMIT 1";
+                    if($result1 = $db['link']->query($query1)){
+                        while($row1 = $result1->fetch_array(MYSQLI_ASSOC)){
+            ?>
+
+            <div class="row-fluid">
+            	<div class="span12">
+            		
+                    <div id="map_canvas" style="width:100%; height:100%"></div>
+                
+                </div>
+            </div>
+            
+            <?php
+                        }
+                    }
+                }
+            ?>
+            
+            
+            
+
+		</div>
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2012 Shop Startup &middot; <a href="#">About Us</a></p>
+      </footer>
+
+</div><!-- /.container -->
+
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo $cfg['weburl']; ?>/js/jquery.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-transition.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-alert.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-modal.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-tab.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-popover.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-button.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo $cfg['weburl']; ?>/js/bootstrap-typeahead.js"></script>
+</body>
+</html>
